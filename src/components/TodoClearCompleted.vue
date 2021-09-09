26a3e17911ae22a6d37546a7c817ad42dfc56999
GUILHERME MICHELS <guilherme.michels@STW.LOCAL>
2021-09-09 14:59:12 -0300

    first commit

---
+++ src/components/TodoClearCompleted.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button class="clearButton" v-if="showClearCompletedButton" @click="clearCompleted">
+      Clear Completed
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo-clear-completed",
+  props: {
+    showClearCompletedButton: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    clearCompleted() {
+      eventBus.$emit("clearCompletedTodos");
+    },
+  },
+};
+</script>
+
+<style>
+
+.clearButton {
+  width: 140px;
+}
+
+</style>
+
